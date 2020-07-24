4b08258427b7e06a8945af8de58bf9feab6b98ab
Alexander <xanderhard@icloud.com>
2020-07-24 20:13:53 +0300

    Update function.go

--- api/modules/function.go
+++ api/modules/function.go
@@ -1,18 +1,32 @@
 package modules
 
 import (
-	nats "github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go"
 	"log"
+	"time"
 )
 
-func Sum(a int, b int) int {
-	return a + b
-}
+const (
+	URL_NATS = "http://nats:4222"
+)
 
-func ConnectNats() {
-	nc, err := nats.Connect(nats.DefaultURL)
+func PingNats() (err error) {
+	nc, err := nats.Connect(URL_NATS, nats.PingInterval(20*time.Second), nats.MaxPingsOutstanding(5))
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	defer nc.Close()
+	return err
+}
+
+func ConnectNats() (nc *nats.Conn, err error) {
+	nc, err = nats.Connect(URL_NATS, nats.Timeout(time.Second*10))
+	log.Println("Connected to " + URL_NATS)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer nc.Close()
+	return nc, err
 }
